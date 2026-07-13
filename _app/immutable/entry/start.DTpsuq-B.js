58a689c7626f0de360071be3eaf4038ca105dbac
Michael Easter <codetojoy@gmail.com>
2026-07-13 19:23:28 -0300

    Updates

---
+++ _app/immutable/entry/start.DTpsuq-B.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"../chunks/CPvfwZkr.js";export{e as load_css,t as start};
--- src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-// Pure client-side PWA: every page is prerendered to static HTML
-// (adapter-static) and all interactivity happens in the browser.
-export const prerender = true;
